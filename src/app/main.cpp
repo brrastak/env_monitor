62db7990675e1b2c72a6cc709ce15cb3e2b0f7bb
Yuri Volkov <volkov.yuri.alex@gmail.com>
2026-07-19 15:51:45 +0200

    Add FreeRTOS

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -2,21 +2,43 @@
 #include "stm32f4xx_hal.h"
 #include "log/log.hpp"
 
+#include "FreeRTOS.h"
+#include "task.h"
+#include "queue.h"
+#include "bsp/led.hpp"
+
+
+static void heartbit_task(void* pvParameters);
 
 int main() {
 
     logging::init();
 
-    auto board = bsp::Board();
-    auto led = board.led;
-    auto iwdg = board.iwdg;
+    static auto board = bsp::Board();
+    static auto led = board.led;
+    static auto iwdg = board.iwdg;
 
     logging::info("Initialization completed");
 
+    uint32_t sysClockFreq = HAL_RCC_GetSysClockFreq();
+    logging::info("System clock frequency: {} Hz", sysClockFreq);
+
+    xTaskCreate(heartbit_task, "Heartbit Task", 100, &led, 1, nullptr);
+
+    vTaskStartScheduler();
+
     while (true) {
-        HAL_Delay(1000);
-        iwdg.feed();
-        led.blink();
+        
+    }
+}
+
+static void heartbit_task(void* pvParameters) {
+    
+    bsp::Led* led = static_cast<bsp::Led*>(pvParameters);
+
+    while (true) {
+        vTaskDelay(pdMS_TO_TICKS(1000));
+        led->blink();
         logging::info("Blink!");
     }
 }
