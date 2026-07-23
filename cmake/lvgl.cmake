5f8930ac1824e8098c831a1599d9598a0ef2eb45
Yuri Volkov <volkov.yuri.alex@gmail.com>
2026-07-23 23:57:38 +0200

    Add lvgl

---
+++ cmake/lvgl.cmake
@@ -0,0 +1,12 @@
+set(LV_BUILD_CONF_PATH "${PROJECT_SOURCE_DIR}/config/lv_conf.h")
+
+add_subdirectory(external/lvgl)
+
+target_compile_definitions(lvgl PRIVATE
+    LV_CONF_INCLUDE_SIMPLE
+)
+
+target_include_directories(lvgl
+    PRIVATE
+        ${PROJECT_SOURCE_DIR}/config
+)
